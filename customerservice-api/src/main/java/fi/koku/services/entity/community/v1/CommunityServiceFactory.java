b63ab03ded16190e0a731d08e2aba65f1d23579d
marko asplund <marko.asplund@ixonos.com>
2011-08-28 16:01:14 +0000

    add service factory classes; change jaxb generation options

---
+++ customerservice-api/src/main/java/fi/koku/services/entity/community/v1/CommunityServiceFactory.java
@@ -0,0 +1,38 @@
+package fi.koku.services.entity.community.v1;
+
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.BindingProvider;
+
+/**
+ * Factory for community service.
+ * 
+ * @author aspluma
+ */
+public class CommunityServiceFactory {
+  private String uid;
+  private String pwd;
+  private String endpointBaseUrl;
+  private String implVersion = "0.0.1-SNAPSHOT";
+  private final URL LOG_WSDL_LOCATION = getClass().getClassLoader().getResource("/wsdl/communityService.wsdl");
+
+  public CommunityServiceFactory(String uid, String pwd, String endpointBaseUrl) {
+    this.uid = uid;
+    this.pwd = pwd;
+    this.endpointBaseUrl = endpointBaseUrl;
+  }
+
+  public CommunityServicePortType getCommunityService() {
+    CommunityService service = new CommunityService(LOG_WSDL_LOCATION, new QName(
+        "http://services.koku.fi/entity/community/v1", "communityService"));
+    CommunityServicePortType port = service.getCommunityServiceSoap11Port();
+    String epAddr = endpointBaseUrl + "/customer-service-" + implVersion + "/CommunityServiceBean";
+
+    ((BindingProvider) port).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, epAddr);
+    ((BindingProvider) port).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, uid);
+    ((BindingProvider) port).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, pwd);
+    return port;
+  }
+
+}
---
+++ customerservice-api/src/main/java/fi/koku/services/entity/customer/v1/CustomerServiceFactory.java
@@ -0,0 +1,38 @@
+package fi.koku.services.entity.customer.v1;
+
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.BindingProvider;
+
+/**
+ * Factory for customer service.
+ * 
+ * @author aspluma
+ */
+public class CustomerServiceFactory {
+  private String uid;
+  private String pwd;
+  private String endpointBaseUrl;
+  private String implVersion = "0.0.1-SNAPSHOT";
+  private final URL LOG_WSDL_LOCATION = getClass().getClassLoader().getResource("/wsdl/customerService.wsdl");
+
+  public CustomerServiceFactory(String uid, String pwd, String endpointBaseUrl) {
+    this.uid = uid;
+    this.pwd = pwd;
+    this.endpointBaseUrl = endpointBaseUrl;
+  }
+
+  public CustomerServicePortType getCustomerService() {
+    CustomerService service = new CustomerService(LOG_WSDL_LOCATION, new QName(
+        "http://services.koku.fi/entity/customer/v1", "customerService"));
+    CustomerServicePortType port = service.getCustomerServiceSoap11Port();
+    String epAddr = endpointBaseUrl + "/customer-service-" + implVersion + "/CustomerServiceBean";
+
+    ((BindingProvider) port).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, epAddr);
+    ((BindingProvider) port).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, uid);
+    ((BindingProvider) port).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, pwd);
+    return port;
+  }
+
+}
