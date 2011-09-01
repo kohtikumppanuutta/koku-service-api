8d6dd843ee6ca35750d90f5ef29b698e1a6afa95
marko asplund <marko.asplund@ixonos.com>
2011-09-01 20:10:19 +0000

    add CCIS service API placeholder

---
+++ sisservice-api/src/main/java/fi/koku/services/entity/sis/v1/Helmi.java
@@ -0,0 +1,12 @@
+package fi.koku.services.entity.sis.v1;
+
+/**
+ * Class for accessing the Helmi student information system.
+ * 
+ * This class currently acts as a dummy implementation representing Helmi access.
+ * 
+ * @author aspluma
+ */
+public class Helmi {
+
+}
