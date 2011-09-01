51e4e114887f6d430acc689e3ac9e097ed74f76b
marko asplund <marko.asplund@ixonos.com>
2011-09-01 19:49:32 +0000

    add HIS service API placeholder

---
+++ hisservice-api/src/main/java/fi/koku/services/entity/his/v1/Pegasos.java
@@ -0,0 +1,12 @@
+package fi.koku.services.entity.his.v1;
+
+/**
+ * Class for accessing the Pegasos healthcare information system.
+ * 
+ * This class currently acts as a dummy implementation representing Pegasos access.
+ * 
+ * @author aspluma
+ */
+public class Pegasos {
+
+}
