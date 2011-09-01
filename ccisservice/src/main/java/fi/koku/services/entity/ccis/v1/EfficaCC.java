851c40e322657d380b7df9b9fd4d95a6531f4305
marko asplund <marko.asplund@ixonos.com>
2011-09-01 20:00:08 +0000

    add CCIS service API placeholder

---
+++ ccisservice/src/main/java/fi/koku/services/entity/ccis/v1/EfficaCC.java
@@ -0,0 +1,12 @@
+package fi.koku.services.entity.ccis.v1;
+
+/**
+ * Class for accessing the Effica child care information system.
+ * 
+ * This class currently acts as a dummy implementation representing Effica access.
+ * 
+ * @author aspluma
+ */
+public class EfficaCC {
+
+}
