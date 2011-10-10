72d51f2a80b38d3d9c2f4ca4b69e708530756eed
Petri Mikkonen <petri.mikkonen@ixonos.com>
2011-10-10 13:41:49 +0000

    Changed method names, added audit-info-params (as string), changed customer/community-queries to use pic-lists, added method for getting persons parents.

--- familyservice-api/src/main/java/fi/koku/services/entity/family/v1/FamilyService.java
+++ familyservice-api/src/main/java/fi/koku/services/entity/family/v1/FamilyService.java
@@ -13,11 +13,15 @@ import fi.koku.services.entity.community.v1.CommunityQueryCriteriaType;
 import fi.koku.services.entity.community.v1.CommunityServiceFactory;
 import fi.koku.services.entity.community.v1.CommunityServicePortType;
 import fi.koku.services.entity.community.v1.CommunityType;
+import fi.koku.services.entity.community.v1.MemberPicsType;
 import fi.koku.services.entity.community.v1.MemberType;
+import fi.koku.services.entity.community.v1.ServiceFault;
+import fi.koku.services.entity.customer.v1.CustomerQueryCriteriaType;
 import fi.koku.services.entity.customer.v1.CustomerServiceFactory;
-import fi.koku.services.entity.customer.v1.CustomerServiceHelper;
 import fi.koku.services.entity.customer.v1.CustomerServicePortType;
 import fi.koku.services.entity.customer.v1.CustomerType;
+import fi.koku.services.entity.customer.v1.CustomersType;
+import fi.koku.services.entity.customer.v1.PicsType;
 import fi.koku.services.entity.family.FamilyConstants;
 
 /**
@@ -43,30 +47,24 @@ public class FamilyService {
     communityService = communityServiceFactory.getCommunityService();
   }
 
-  public List<CustomerType> getUsersChildren(String pic) throws Exception {
+  /**
+   * Returns children of the person
+   * 
+   * @param pic, pic of person
+   * @param auditUserId
+   * @param auditComponentId
+   * @return List of children (CustomerType)
+   * @throws Exception
+   */
+  public List<CustomerType> getPersonsChildren(String pic, String auditUserId, String auditComponentId) throws Exception {
 
     List<CustomerType> children = new ArrayList<CustomerType>();
     List<CommunityType> communities = null;
 
     try {
 
-      // 1) Query communities and users
-
-      // Set community query audit info
-      fi.koku.services.entity.community.v1.AuditInfoType communityAuditInfoType = new fi.koku.services.entity.community.v1.AuditInfoType();
-      communityAuditInfoType.setComponent(FamilyConstants.COMPONENT_FAMILY_SERVICE);
-      communityAuditInfoType.setUserId(pic);
-
-      // Add community query parameters
-      CommunityQueryCriteriaType criteria = new CommunityQueryCriteriaType();
-      criteria.setMemberPic(pic);
-      // criteria.setCommunityType(FamilyConstants.COMMUNITY_TYPE_FAMILY);
-      criteria.setCommunityType(FamilyConstants.COMMUNITY_TYPE_GUARDIAN_COMMUNITY);
-
-      communities = communityService.opQueryCommunities(criteria, communityAuditInfoType).getCommunity();
-      LOG.debug("Tried to opQueryCommunities(criteria{memberPic=" + pic + " and communityType="
-          + FamilyConstants.COMMUNITY_TYPE_FAMILY + "}," + "auditInfo{userId=" + communityAuditInfoType.getUserId()
-          + " and componentType=" + communityAuditInfoType.getComponent() + "}) = list.size=" + communities.size());
+      // 1) Query communities and persons
+      communities = searchPersonsCommunitiesByPic(pic, auditUserId, auditComponentId);
 
       // ##TODO## The query may return more than one communities, how to
       // determine which is the correct one?
@@ -75,43 +73,130 @@ public class FamilyService {
       if (communities.size()==1) {
         final int THE_ONLY_COMMUNITY = 0;
         List<MemberType> members = communities.get(THE_ONLY_COMMUNITY).getMembers().getMember();
-        List<String> memberPics = getChildrenPics(members);
+        List<String> memberPics = filterMemberPicsWithRole(members, FamilyConstants.ROLE_DEPENDANT);
 
-        // 2) Query users (CustomerType) in community
-        fi.koku.services.entity.customer.v1.AuditInfoType customerAuditInfoType = new fi.koku.services.entity.customer.v1.AuditInfoType();
-        customerAuditInfoType.setComponent(FamilyConstants.COMPONENT_FAMILY_SERVICE);
-        customerAuditInfoType.setUserId(pic);
-
-        CustomerServiceHelper srvHelper = new CustomerServiceHelper(customerService, customerAuditInfoType);
-        children = srvHelper.getCustomers(memberPics);
+        // 2) Query persons (CustomerType) in community
+        children = searchPersonsByPicList(memberPics, auditUserId, auditComponentId);
 
       }else if(communities.size()>1){  
-        throw new Exception("User has communities.size="+communities.size()+" (not required 1).");//#TODO# Evaluate proper operation for this
+        throw new Exception("Person has communities.size="+communities.size()+" (not required 1).");//#TODO# Evaluate proper operation for this
       }else{
         LOG.debug("No communities found with pic="+pic); 
       }
     
     
     } catch (fi.koku.services.entity.community.v1.ServiceFault communityFault) {
-      LOG.error("Failed to get users children (community members) from CommunityService", communityFault);
+      LOG.error("Failed to get persons children (community members) from CommunityService", communityFault);
     }
     return children;
   }
 
   /**
-   * Extracts pics of children (=has role dependant) from MemberType-list
+   * Return parents of the person
+   * 
+   * @param pic
+   * @param auditUserId
+   * @param auditComponentId
+   * @return List of parents (CustomerType)
+   * @throws Exception
+   */
+  public List<CustomerType> getPersonsParents(String pic, String auditUserId, String auditComponentId) throws Exception {
+
+    List<CustomerType> parents = new ArrayList<CustomerType>();
+    List<CommunityType> communities = null;
+
+    try {
+
+      // 1) Query communities and persons
+      communities = searchPersonsCommunitiesByPic(pic, auditUserId, auditComponentId);
+
+      // ##TODO## The query may return more than one communities, how to
+      // determine which is the correct one?
+      // ##TODO## Assuming that the first list is the one. Check this!
+      //
+      if (communities.size()==1) {
+        final int THE_ONLY_COMMUNITY = 0;
+        List<MemberType> members = communities.get(THE_ONLY_COMMUNITY).getMembers().getMember();
+        List<String> memberPics = filterMemberPicsWithRole(members, FamilyConstants.ROLE_GUARDIAN);
+
+        parents = searchPersonsByPicList(memberPics, auditUserId, auditComponentId);
+
+      }else if(communities.size()>1){  
+        throw new Exception("Person has communities.size="+communities.size()+" (not required 1).");//#TODO# Evaluate proper operation for this
+      }else{
+        LOG.debug("No communities found with pic="+pic); 
+      }
+    
+    
+    } catch (fi.koku.services.entity.community.v1.ServiceFault communityFault) {
+      LOG.error("Failed to get persons parents (community members) from CommunityService", communityFault);
+    }
+    return parents;
+  }
+  
+  
+  private List<CustomerType> searchPersonsByPicList(List<String> memberPics, String auditUserId, String auditComponentId)
+      throws fi.koku.services.entity.customer.v1.ServiceFault {
+    CustomerQueryCriteriaType query = new CustomerQueryCriteriaType();
+    
+    fi.koku.services.entity.customer.v1.AuditInfoType customerAuditInfoType = new fi.koku.services.entity.customer.v1.AuditInfoType();
+    customerAuditInfoType.setComponent(auditComponentId);
+    customerAuditInfoType.setUserId(auditUserId);
+
+    //Add pics to criteria
+    PicsType picsType = new PicsType();
+    for (String memberPic : memberPics) {
+      picsType.getPic().add(memberPic);  
+    }
+    query.setPics(picsType);
+    CustomersType customersType = customerService.opQueryCustomers(query, customerAuditInfoType);
+    return customersType.getCustomer();
+  }
+
+
+  
+  
+  private List<CommunityType> searchPersonsCommunitiesByPic(String pic, String auditUserId, String auditComponentId)
+      throws ServiceFault {
+    
+    List<CommunityType> communities;
+    // Set community query audit info
+    fi.koku.services.entity.community.v1.AuditInfoType communityAuditInfoType = new fi.koku.services.entity.community.v1.AuditInfoType();
+    communityAuditInfoType.setComponent(auditComponentId);
+    communityAuditInfoType.setUserId(auditUserId);//##TODO## Check if this is correct?
+
+    // Add community query parameters
+    CommunityQueryCriteriaType criteria = new CommunityQueryCriteriaType();
+    
+    // Query with only one pic
+    MemberPicsType picsType = new MemberPicsType();
+    picsType.getMemberPic().add(pic);
+    criteria.setMemberPics(picsType);
+    
+    criteria.setCommunityType(FamilyConstants.COMMUNITY_TYPE_GUARDIAN_COMMUNITY);
+
+    communities = communityService.opQueryCommunities(criteria, communityAuditInfoType).getCommunity();
+    LOG.debug("Tried to opQueryCommunities(criteria{memberPic=" + pic + " and communityType="
+        + FamilyConstants.COMMUNITY_TYPE_GUARDIAN_COMMUNITY + "}," + "auditInfo{userId=" + communityAuditInfoType.getUserId()
+        + " and componentType=" + communityAuditInfoType.getComponent() + "}) = list.size=" + communities.size());
+    return communities;
+  }
+
+  /**
+   * Extracts pics that have given role (ex. has role dependant/guardian) from MemberType-list
    * 
    * @param members, list of MemberTypes
-   * @return, list of Pics as Strings
+   * @param roleId, role as string format
+   * @return pics, list of Pics as Strings
    */
-  private List<String> getChildrenPics(List<MemberType> members) {
+  private List<String> filterMemberPicsWithRole(List<MemberType> members, final String roleId) {
     List<String> pics = null;
-    if (members != null) {
+    if (members != null && roleId!=null) {
       pics = new ArrayList<String>(members.size());
       
       for (MemberType m : members) {
-        // Check if the user has role DEPENDANT
-        if (FamilyConstants.ROLE_DEPENDANT.equals(m.getRole())) {
+        // Check if the person has given roleId
+        if (roleId.equals(m.getRole())) {
           if (m.getPic() != null) {
             pics.add(m.getPic());
           }
