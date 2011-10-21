ad83dac3ea3f2d9527ea6194bad41e890634eca3
Santtu Laukkanen <santtu.laukkanen@ixonos.com>
2011-10-21 13:33:20 +0000

    KOKU-617

--- kksservice-api/src/main/java/fi/koku/services/entity/kks/v1/KksServiceFactory.java
+++ kksservice-api/src/main/java/fi/koku/services/entity/kks/v1/KksServiceFactory.java
@@ -21,7 +21,6 @@ public class KksServiceFactory {
   private String endpointBaseUrl;
 
   private final URL KKS_WSDL_LOCATION = getClass().getClassLoader().getResource("/wsdl/kksService.wsdl");
-  private static final String KKS_SERVICE_VERSION = "0.0.1-SNAPSHOT";
 
   public KksServiceFactory(String uid, String pwd, String endpointBaseUrl) {
     this.uid = uid;
@@ -33,7 +32,7 @@ public class KksServiceFactory {
     KksService service = new KksService(KKS_WSDL_LOCATION, new QName("http://services.koku.fi/entity/kks/v1",
         "kksService"));
     KksServicePortType kksServicePort = service.getKksServiceSoap11Port();
-    String epAddr = endpointBaseUrl + "/kks-service-" + KKS_SERVICE_VERSION + "/KksServiceEndpointBean";
+    String epAddr = endpointBaseUrl + "/KksServiceEndpointBean";
 
     ((BindingProvider) kksServicePort).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, epAddr);
     ((BindingProvider) kksServicePort).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, uid);
