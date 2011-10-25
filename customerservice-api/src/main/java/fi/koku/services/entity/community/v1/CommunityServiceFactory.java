4054a66931b63c158c3d3f2bbbc3aaa7a94d4994
marko asplund <marko.asplund@ixonos.com>
2011-10-25 05:08:25 +0000

    communityservice: fix bug in ws endpoint generation; add diagnostic msgs

--- customerservice-api/src/main/java/fi/koku/services/entity/community/v1/CommunityServiceFactory.java
+++ customerservice-api/src/main/java/fi/koku/services/entity/community/v1/CommunityServiceFactory.java
@@ -5,6 +5,9 @@ import java.net.URL;
 import javax.xml.namespace.QName;
 import javax.xml.ws.BindingProvider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Factory for community service.
  * 
@@ -14,8 +17,8 @@ public class CommunityServiceFactory {
   private String uid;
   private String pwd;
   private String endpointBaseUrl;
-  private String implVersion = "0.0.1-SNAPSHOT";
   private final URL wsdlLocation = getClass().getClassLoader().getResource("wsdl/communityService.wsdl");
+  private static Logger log = LoggerFactory.getLogger(CommunityServiceFactory.class);
 
   public CommunityServiceFactory(String uid, String pwd, String endpointBaseUrl) {
     this.uid = uid;
@@ -24,10 +27,13 @@ public class CommunityServiceFactory {
   }
 
   public CommunityServicePortType getCommunityService() {
+    if(wsdlLocation == null)
+      log.error("wsdllocation=null");
     CommunityService service = new CommunityService(wsdlLocation, new QName(
         "http://services.koku.fi/entity/community/v1", "communityService"));
     CommunityServicePortType port = service.getCommunityServiceSoap11Port();
-    String epAddr = endpointBaseUrl + "/customer-service-" + implVersion + "/CommunityServiceEndpointBean";
+    String epAddr = endpointBaseUrl + "/CommunityServiceEndpointBean";
+    log.debug("ep addr: "+epAddr);
 
     ((BindingProvider) port).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, epAddr);
     ((BindingProvider) port).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, uid);
