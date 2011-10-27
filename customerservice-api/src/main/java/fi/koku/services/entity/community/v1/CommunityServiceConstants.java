05030bf2a782a3a7d67985aafc06cf0f0b5776e7
Mikko Hurula <mikko.hurula@ixonos.com>
2011-10-27 11:13:51 +0000

    adding community service constants

---
+++ customerservice-api/src/main/java/fi/koku/services/entity/community/v1/CommunityServiceConstants.java
@@ -0,0 +1,17 @@
+package fi.koku.services.entity.community.v1;
+
+public class CommunityServiceConstants {
+  final public static String COMMUNITY_TYPE_GUARDIAN_COMMUNITY = "guardian_community";
+  final public static String COMMUNITY_TYPE_FAMILY = "family_community";
+  
+  final public static String COMMUNITY_ROLE_DEPENDANT = "dependant";
+  final public static String COMMUNITY_ROLE_GUARDIAN = "guardian";
+  final public static String COMMUNITY_ROLE_PARENT = "parent";
+  final public static String COMMUNITY_ROLE_FATHER = "father";
+  final public static String COMMUNITY_ROLE_MOTHER = "mother";
+  final public static String COMMUNITY_ROLE_CHILD = "child";
+  
+  final public static String MEMBERSHIP_REQUEST_STATUS_NEW = "new";
+  final public static String MEMBERSHIP_REQUEST_STATUS_APPROVED = "approved";
+  final public static String MEMBERSHIP_REQUEST_STATUS_REJECTED = "rejected";
+}
