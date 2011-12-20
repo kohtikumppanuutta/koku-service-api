3f5f00c2f12aff33cc201fcff50b6bf92368a2f7
Antti Hanhineva <antti.hanhineva@ixonos.com>
2011-12-20 11:58:05 +0000

    KOKU-917 Koodikommentointikorjausten toteutus

--- lokservice-api/src/main/java/fi/koku/services/utility/log/v1/LogServiceFactory.java
+++ lokservice-api/src/main/java/fi/koku/services/utility/log/v1/LogServiceFactory.java
@@ -20,41 +20,38 @@ import org.slf4j.LoggerFactory;
 
 
 /**
+ * Simple factory class to provide access to Log service.
+ * 
  * @author mikkope
- *
  */
 public class LogServiceFactory {
 
+  private String uid;
+  private String pwd;
+  private String endpointBaseUrl;
+  private final URL wsdlLocation = getClass().getClassLoader().getResource("wsdl/logService.wsdl");
 
-private String uid;
-private String pwd;
-private String endpointBaseUrl;
-private final URL wsdlLocation = getClass().getClassLoader().getResource("wsdl/logService.wsdl");
+  private static Logger log = LoggerFactory.getLogger(LogServiceFactory.class);
 
-private static Logger log = LoggerFactory.getLogger(LogServiceFactory.class);
+  public LogServiceFactory(String uid, String pwd, String endpointBaseUrl) {
+    this.uid = uid;
+    this.pwd = pwd;
+    this.endpointBaseUrl = endpointBaseUrl;
+  }
 
-public LogServiceFactory(String uid, String pwd, String endpointBaseUrl) {
-  this.uid = uid;
-  this.pwd = pwd;
-  this.endpointBaseUrl = endpointBaseUrl;
-}
+  public LogServicePortType getLogService() {
+    if (wsdlLocation == null)
+      log.error("wsdllocation=null");
+    LogService service = new LogService(wsdlLocation, new QName("http://services.koku.fi/utility/log/v1", "logService"));
+    LogServicePortType port = service.getLogServiceSoap11Port();
+    String epAddr = endpointBaseUrl + "/LogServiceEndpointBean";
+    log.debug("ep addr: " + epAddr);
 
+    ((BindingProvider) port).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, epAddr);
+    ((BindingProvider) port).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, uid);
+    ((BindingProvider) port).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, pwd);
+    log.debug("Created logServiceClient with epAddr=" + epAddr + ", uid=" + uid);
 
-public LogServicePortType getLogService() {
-  if(wsdlLocation == null)
-    log.error("wsdllocation=null");
-  LogService service = new LogService(wsdlLocation, new QName(
-      "http://services.koku.fi/utility/log/v1", "logService"));
-  LogServicePortType port = service.getLogServiceSoap11Port();
-  String epAddr = endpointBaseUrl + "/LogServiceEndpointBean";
-  log.debug("ep addr: "+epAddr);
-  
-  ((BindingProvider) port).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, epAddr);
-  ((BindingProvider) port).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, uid);
-  ((BindingProvider) port).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, pwd);
-  log.debug("Created logServiceClient with epAddr="+epAddr+", uid="+uid);
-  
-  return port;
-}
-
+    return port;
+  }
 }
