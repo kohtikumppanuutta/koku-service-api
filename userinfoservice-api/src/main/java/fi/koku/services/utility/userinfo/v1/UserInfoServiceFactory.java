fabfb9acd99c477b4c53528828aea2a7ee0f019d
Antti Hanhineva <antti.hanhineva@ixonos.com>
2012-02-03 06:50:33 +0000

    Renamed employeeservice to userinfoservice. Added the domain concept for both queries.

---
+++ userinfoservice-api/src/main/java/fi/koku/services/utility/userinfo/v1/UserInfoServiceFactory.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2011 Ixonos Plc, Finland. All rights reserved.
+ * 
+ * You should have received a copy of the license text along with this program.
+ * If not, please contact the copyright holder (http://www.ixonos.com/).
+ * 
+ */
+package fi.koku.services.utility.userinfo.v1;
+
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.BindingProvider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import fi.koku.services.utility.user.v1.UserInfoService;
+import fi.koku.services.utility.user.v1.UserInfoServicePortType;
+
+/**
+ * Simple factory class to provide access to User Info service.
+ * 
+ * @author hanhian
+ */
+public class UserInfoServiceFactory {
+
+  private String uid;
+  private String pwd;
+  private String endpointBaseUrl;
+  private final URL wsdlLocation = getClass().getClassLoader().getResource("wsdl/userInfoService.wsdl");
+
+  private static Logger log = LoggerFactory.getLogger(UserInfoServiceFactory.class);
+
+  public UserInfoServiceFactory(String uid, String pwd, String endpointBaseUrl) {
+    this.uid = uid;
+    this.pwd = pwd;
+    this.endpointBaseUrl = endpointBaseUrl;
+  }
+
+  public UserInfoServicePortType getUserInfoService() {
+    if (wsdlLocation == null)
+      log.error("wsdllocation=null");
+
+    UserInfoService service = new UserInfoService(wsdlLocation, new QName(
+        "http://services.koku.fi/utility/user/v1", "userInfoService"));
+    UserInfoServicePortType port = service.getUserInfoServiceSoap11Port();
+    String epAddr = endpointBaseUrl + "/UserInfoServiceEndpointBean";
+    log.debug("ep addr: " + epAddr);
+
+    ((BindingProvider) port).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, epAddr);
+    ((BindingProvider) port).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, uid);
+    ((BindingProvider) port).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, pwd);
+    log.debug("Created userInfoServiceClient with epAddr=" + epAddr + ", uid=" + uid);
+
+    return port;
+  }
+}
